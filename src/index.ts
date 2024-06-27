8bc9f3b65919514d514c1896f3d912c1459578c5
jeancarrie-kima <jean@kima.finance>
2024-06-27 11:54:11 +0800

    add htlc lock request

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,10 @@ import CookieParser from 'cookie-parser'
 import { v4 as uuidv4 } from 'uuid'
 import { validate } from './validate'
 import { RiskResult, RiskScore, getRisk, RiskScore2String } from './xplorisk'
-import { submitKimaTransaction } from '@kimafinance/kima-transaction-api'
+import {
+  submitKimaTransaction,
+  submitHtlcLock
+} from '@kimafinance/kima-transaction-api'
 import { fetchWrapper } from './fetch-wrapper'
 import { Network, validate as validateBTC } from 'bitcoin-address-validation'
 
@@ -228,6 +231,60 @@ function hexStringToUint8Array(hexString: string) {
   }
   return arrayBuffer
 }
+
+app.post('/htlc', authenticateJWT, async (req: Request, res: Response) => {
+  const {
+    fromAddress,
+    senderPubkey,
+    amount,
+    htlcTimeout,
+    htlcAddress,
+    txHash
+  } = req.body
+
+  console.log(req.body)
+
+  if (process.env.XPLORISK_URL) {
+    try {
+      const results: Array<RiskResult> = await getRisk([fromAddress])
+
+      const totalRisky: number = results.reduce(
+        (a, c) => a + (c.risk_score !== RiskScore.LOW ? 1 : 0),
+        0
+      )
+      if (totalRisky > 0) {
+        let riskyResult = ''
+        for (let i = 0; i < results.length; i++) {
+          if (results[i].risk_score === RiskScore.LOW) continue
+          if (riskyResult.length > 0) riskyResult += ', '
+          riskyResult += `${results[i].address} has ${
+            RiskScore2String[results[i].risk_score]
+          } risk`
+        }
+        res.status(500).send(riskyResult)
+        return
+      }
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
+  try {
+    const result = await submitHtlcLock({
+      fromAddress,
+      amount,
+      htlcTimeout,
+      htlcAddress,
+      txHash,
+      senderPubkey
+    })
+    console.log(result)
+    res.send(result)
+  } catch (e) {
+    console.log(e)
+    res.status(500).send('failed to submit transaction')
+  }
+})
 
 app.post('/submit', authenticateJWT, async (req: Request, res: Response) => {
   const {
