4a443e001a165226449d0f49d58ffb02a6c45658
mayjer-kima <jeremy.may@kima.finance>
2025-02-25 15:09:08 -0800

    remove same-origin middleware - The same-orgin code duplicates the CORS config - Fix CORS related error for localhost

--- src/middleware/cors.ts
+++ src/middleware/cors.ts
@@ -1,29 +1,33 @@
 import cors from 'cors'
+import { toUrl } from '../utils'
 
 export const corsConfig = cors({
   origin: (origin, callback) => {
     if (
       process.env.NODE_ENV === 'test' ||
-      (process.env.NODE_ENV === 'development' && !origin)
+      process.env.NODE_ENV === 'development' ||
+      !origin
     ) {
+      // allow requests from localhost and test environments
+      // attempting to block localhost with CORS is not effective
+      // use IP whitelisting instead
       callback(null, true)
       return
     }
 
-    if (!origin) {
-      callback(null, true)
-      return
-    }
+    try {
+      const originHostname = toUrl(origin).hostname
+      const domains = (process.env.DOMAIN as string).split(',')
 
-    const originHostname = new URL(origin as string).hostname
-    const domains = (process.env.DOMAIN as string).split(',')
-
-    for (const domain of domains.filter((domain) => !!domain)) {
-      const domainHostname = new URL(domain).hostname
-      if (originHostname.endsWith(domainHostname)) {
-        callback(null, true)
-        return
+      for (const domain of domains.filter((domain) => !!domain)) {
+        const domainHostname = toUrl(domain).hostname
+        if (originHostname.endsWith(domainHostname)) {
+          callback(null, true)
+          return
+        }
       }
+    } catch (e) {
+      console.error('corsConfig: error', e)
     }
 
     callback(new Error('Not allowed by CORS'))
