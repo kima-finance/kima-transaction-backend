0682970a50924efde7d5c9ed08e213ed05ecef38
mayjer-kima <jeremy.may@kima.finance>
2025-04-07 13:23:00 -0700

    chore: remove CORS credentials include and cookie parser as not needed - Switch some dependancies to dev

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,5 @@
 import './env-validate'
 import express, { Express } from 'express'
-import CookieParser from 'cookie-parser'
 import helmet from 'helmet'
 import morgan from 'morgan'
 import './bigint-shim'
@@ -15,7 +14,6 @@ const app: Express = express()
 // middleware
 app.use(corsConfig)
 app.use(helmet())
-app.use(CookieParser())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(morgan(isDev ? 'dev' : 'combined'))
